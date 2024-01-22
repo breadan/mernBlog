7bfcefc95cbb197e8adfd97e935e76dd95fe0951
breadan <breadan2020@example.com>
2024-01-22 23:19:40 +0200

    editConnection

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const url = process.env.MONGO_URL;
 
 const options = {
   autoIndex: false, // Don't build indexes
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  serverSelectionTimeoutMS: 7000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   family: 4, // Use IPv4, skip trying IPv6
 };
