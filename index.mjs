95487574910f9b6a2d60947c008ef9c403101bc2
UrCodeBuddy <59134280+xUrCodeBuddyx@users.noreply.github.com>
2024-01-27 20:32:01 +0100

    Add swagger documentation and dependencies

--- index.mjs
+++ index.mjs
@@ -1,27 +1,52 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv"; //secure 1
-import "./config/connection.js";
-import authRouter from "./src/routes/auth.routs.js";
-import userRouter from "./src/routes/user.rout.js";
-import { ApiError } from "./src/utils/apiError.js"
-import { globalError } from "./src/middlewares/err.middlware.js";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
-
-const port =  process.env.PORT || 7000
-const mode = process.env.NODE_ENV 
+import './config/connection.js';
+import authRouter from './src/routes/auth.routs.js';
+import userRouter from './src/routes/user.rout.js';
+import { ApiError } from './src/utils/apiError.js';
+import { globalError } from './src/middlewares/err.middlware.js';
+import { readFile } from 'fs/promises';
+const port = process.env.PORT || 7000;
+const mode = process.env.NODE_ENV;
 //secure 2
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+async function loadJson() {
+  const data = await readFile(
+    new URL('./docs/swagger.json', import.meta.url),
+    'utf8'
+  );
+  return JSON.parse(data);
+}
+
+const internalDocs = await loadJson();
+
+const options = {
+  swaggerOptions: {
+    persistAuthorization: true,
+    title: 'Best Wallet API',
+  },
+  customCss: '.topbar { display: none }',
+};
+
+// Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(internalDocs, options));
+
+// Routes
 app.use(authRouter);
 app.use(userRouter);
 // app.use("/v1/messages", messageRouter);
-app.get("/", (req, res) => res.send(" World!"));
+app.get('/', (req, res) => res.send(' World!'));
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   // const err = new Error(`Cant find this rout: ${req.originalUrl}`);
   // next(err.message);
   next(new ApiError(`Cant find this rout: ${req.originalUrl}`, 400));
@@ -29,7 +54,6 @@ app.all("*", (req, res, next) => {
 
 //Global error handler
 app.use(globalError);
-
 
 app.listen(port, () => {
   console.log(`Example app running on port ${port} mode: ${mode}! ^_^ `);
